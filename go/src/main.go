802d8895c8bf77d901b0196d5c7a1daed8f0497f
97631431+kkassim1@users.noreply.github.com <97631431+kkassim1@users.noreply.github.com>
2022-04-05 17:02:42 -0700

    Student updates

--- go/src/main.go
+++ go/src/main.go
@@ -11,48 +11,48 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type Movie struct {
-	ID       string    `json:"id"`
-	Isbn     string    `json:"isbn"`
-	Title    string    `json:"title"`
-	Director *Director `json:"director"`
+type Student struct {
+	ID          string   `json:"id"`
+	Class       string   `json:"isbn"`
+	StudentName string   `json:"title"`
+	Teacher     *Teacher `json:"director"`
 }
 
-type Director struct {
+type Teacher struct {
 	Firstname string `json:"firstname"`
 	Lastname  string `json:"lastname"`
 }
 
-var movies []Movie
+var Students []Student
 
-func getMovies(w http.ResponseWriter, r *http.Request) {
+func getStudents(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(movies)
+	json.NewEncoder(w).Encode(Students)
 
 }
 
-func deleteMovie(w http.ResponseWriter, r *http.Request) {
+func deleteStudent(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 
-	for index, item := range movies {
+	for index, item := range Students {
 
 		if item.ID == params["id"] {
-			movies = append(movies[:index], movies[index+1:]...)
+			Students = append(Students[:index], Students[index+1:]...)
 			break
 		}
 	}
-	json.NewEncoder(w).Encode(movies)
+	json.NewEncoder(w).Encode(Students)
 }
 
-func getMovie(w http.ResponseWriter, r *http.Request) {
+func getStudent(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 
-	for _, item := range movies {
+	for _, item := range Students {
 		if item.ID == params["id"] {
 
 			json.NewEncoder(w).Encode(item)
@@ -64,33 +64,33 @@ func getMovie(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func createMovie(w http.ResponseWriter, r *http.Request) {
+func createStudent(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
-	var movie Movie
+	var student Student
 
-	_ = json.NewDecoder(r.Body).Decode(&movie)
-	movie.ID = strconv.Itoa(rand.Intn(100000000))
-	movies = append(movies, movie)
-	json.NewEncoder(w).Encode(movie)
+	_ = json.NewDecoder(r.Body).Decode(&student)
+	student.ID = strconv.Itoa(rand.Intn(100000000))
+	Students = append(Students, student)
+	json.NewEncoder(w).Encode(student)
 
 }
 
-func updateMovie(w http.ResponseWriter, r *http.Request) {
+func updateStudent(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 
-	for index, item := range movies {
+	for index, item := range Students {
 
 		if item.ID == params["id"] {
-			movies = append(movies[:index], movies[index+1:]...)
-			var movie Movie
+			Students = append(Students[:index], Students[index+1:]...)
+			var student Student
 
-			_ = json.NewDecoder(r.Body).Decode(&movie)
-			movie.ID = params["id"]
-			movies = append(movies, movie)
-			json.NewEncoder(w).Encode(movie)
+			_ = json.NewDecoder(r.Body).Decode(&student)
+			student.ID = params["id"]
+			Students = append(Students, student)
+			json.NewEncoder(w).Encode(student)
 		}
 
 	}
@@ -101,17 +101,17 @@ func main() {
 
 	r := mux.NewRouter()
 
-	movies = append(movies, Movie{ID: "1", Isbn: "32122", Title: "movie 1", Director: &Director{Firstname: "john", Lastname: "doe"}})
+	Students = append(Students, Student{ID: "1", Class: "32122", StudentName: "movie 1", Teacher: &Teacher{Firstname: "john", Lastname: "doe"}})
 
-	movies = append(movies, Movie{ID: "2", Isbn: "32122", Title: "movie 2", Director: &Director{Firstname: "kkn", Lastname: "yup"}})
-	r.HandleFunc("/movies", getMovies).Methods("GET")
-	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	Students = append(Students, Student{ID: "2", Class: "32122", StudentName: "movie 2", Teacher: &Teacher{Firstname: "kkn", Lastname: "yup"}})
+	r.HandleFunc("/movies", getStudents).Methods("GET")
+	r.HandleFunc("/movies/{id}", getStudent).Methods("GET")
 
-	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies", createStudent).Methods("POST")
 
-	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", updateStudent).Methods("PUT")
 
-	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+	r.HandleFunc("/movies/{id}", deleteStudent).Methods("DELETE")
 
 	fmt.Print("starting server at port 8000\n")
 	log.Fatal(http.ListenAndServe(":8000", r))
